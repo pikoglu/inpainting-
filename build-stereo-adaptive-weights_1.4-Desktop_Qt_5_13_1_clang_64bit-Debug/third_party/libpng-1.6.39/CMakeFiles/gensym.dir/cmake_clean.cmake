5c9eae4efb71c51465c5b495d0390b6bac73ec67
Félix Fourreau <felix.fourreau@eleves.enpc.fr>
2024-06-20 14:46:22 +0200

    suppression des build initalement commit

--- build-stereo-adaptive-weights_1.4-Desktop_Qt_5_13_1_clang_64bit-Debug/third_party/libpng-1.6.39/CMakeFiles/gensym.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gensym"
-  "libpng.sym"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gensym.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build-stereo-adaptive-weights_1.4-Desktop_Qt_5_13_1_clang_64bit-Debug/third_party/tiff-4.0.10/libtiff/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/felixfourreau/Desktop/projet_vacances/stereo-adaptive-weights_1.4/third_party/tiff-4.0.10/libtiff
-# Build directory: /Users/felixfourreau/Desktop/projet_vacances/build-stereo-adaptive-weights_1.4-Desktop_Qt_5_13_1_clang_64bit-Debug/third_party/tiff-4.0.10/libtiff
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
